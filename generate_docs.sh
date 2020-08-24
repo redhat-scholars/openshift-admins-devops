f8fb51e0e34eaf6df85b09af1ac0421716832784
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-08-24 13:12:52 +0530

    (WIP) Pipelines
     - Add Pipelines and Triggers
     - use full url for devsite, helps PR review
     - Tekton Pipelines now refer to latest TP1.1

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-docker run -v `pwd`:/antora --rm -t antora/antora:2.3.3 --pull --stacktrace github-pages-site.yml
+_CURR_DIR="$( cd "$(dirname "$0")" || return ; pwd -P )"
+
+rm -rf "$_CURR_DIR/gh-pages" "$_CURR_DIR/.cache"
+
+docker run -v "${_CURR_DIR}:/antora" --rm -t antora/antora:2.3.3 --pull --stacktrace site.yml
